94ae7a93eaa9b629a1993ed5af3fabae42cf1cf4
Julian James <julian@Julians-MacBook-Pro.local>
2022-01-17 14:20:59 +0000

    changed procfile

--- api/model/User.js
+++ api/model/User.js
@@ -1,35 +1,35 @@
 const mongoose = require('mongoose');
 
-module.exports = class User {
-    constructor(data) {
-        this.user_id = data.user_id,
-        this.username = data.username,
-        this.email = data.email,
-        this.password = data.password
-    }
+// module.exports = class User {
+//     constructor(data) {
+//         this.user_id = data.user_id,
+//         this.username = data.username,
+//         this.email = data.email,
+//         this.password = data.password
+//     }
 
-    static get All(){
-        const users = Data.users.map((user) => new Post(user));
-        return posts;
-    };
+//     static get All(){
+//         const users = Data.users.map((user) => new Post(user));
+//         return posts;
+//     };
 
-    static findById(id){
-        try{
-            const postData = Data.posts.filter((post) => post.id === id)[0];
-            const post = new Post(postData);
-            return post;
-        } catch (err) {
-            throw new Error('This post does not exist.');
-        }
-    };
+//     static findById(id){
+//         try{
+//             const postData = Data.posts.filter((post) => post.id === id)[0];
+//             const post = new Post(postData);
+//             return post;
+//         } catch (err) {
+//             throw new Error('This post does not exist.');
+//         }
+//     };
     
-    static create(post){
-        const newPostId = Data.posts.length;
-        const newPost = new Post({id: newPostId, title: '', body:'', date: new Date().toUTCString(), likes1: "0", likes2: "0", likes3: "0", gif: "", ...post});
-        Data.posts.push(newPost);
-        return newPost;
-    };
-};
+//     static create(post){
+//         const newPostId = Data.posts.length;
+//         const newPost = new Post({id: newPostId, title: '', body:'', date: new Date().toUTCString(), likes1: "0", likes2: "0", likes3: "0", gif: "", ...post});
+//         Data.posts.push(newPost);
+//         return newPost;
+//     };
+// };
 
 
 
