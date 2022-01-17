58016ab309a5a963995211641f9ca042c85c7048
Julian James <julian@Julians-MacBook-Pro.local>
2022-01-17 17:02:48 +0000

    added some CRUD operations to routes/habits.js

--- api/routes/habits.js
+++ api/routes/habits.js
@@ -27,6 +27,38 @@ router.get('/habits', async (req, res) => {
     }
 });
 
+//CREATE ROUTE
+app.post("/habits", async (req, res) => {
+    const habit = new User(req.body);
+  
+    try {
+      await habit.save();
+      res.send(habit);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
 
+//UPDATE ROUTE
+app.patch("/habits/:id", async (req, res) => {
+    try {
+      await User.findByIdAndUpdate(req.params.id, req.body);
+      await User.save();
+      res.send(habit);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
+
+app.delete("/habits/:id", async (req, res) => {
+    try {
+      const habit = await User.findByIdAndDelete(req.params.id);
+  
+      if (!habit) res.status(404).send("No item found");
+      res.status(200).send();
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
 
 module.exports = router;
