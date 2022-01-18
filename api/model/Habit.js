e92fa1513e69fb4cd0ecbbfe3a5bcad754d22210
Julian James <julian@Julians-MacBook-Pro.local>
2022-01-18 09:26:05 +0000

    added habit mongoos model

--- api/model/Habit.js
+++ api/model/Habit.js
@@ -1,66 +1,26 @@
-// module.exports = class Habit {
-//     constructor(data){
-//         this.id = data.id;
-//         this.name = data.name;
-//         this.date = data.date;
-//         this.email = data.email;
-//         this.likes1 = data.likes1;
-//         this.likes2 = data.likes2;
-//         this.likes3 = data.likes3;
-//         this.gif = data.gif
-//     };
-
-//     static get All(){
-//         const posts = Data.posts.map((post) => new Post(post));
-//         return posts;
-//     };
-
-//     static findById(id){
-//         try{
-//             const postData = Data.posts.filter((post) => post.id === id)[0];
-//             const post = new Post(postData);
-//             return post;
-//         } catch (err) {
-//             throw new Error('This post does not exist.');
-//         }
-//     };
-    
-//     static create(post){
-//         const newPostId = Data.posts.length;
-//         const newPost = new Post({id: newPostId, title: '', body:'', date: new Date().toUTCString(), likes1: "0", likes2: "0", likes3: "0", gif: "", ...post});
-//         Data.posts.push(newPost);
-//         return newPost;
-//     };
-// };
-
+const mongoose = require('mongoose');
 
 const habitSchema = new mongoose.Schema({
-    nodejs: {
+    Habits: {
         type: String,
         required: true,
-        min: 6,
+        min: 3,
         max: 255
     },
-    React: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 255
+    Frequency: {
+        type: Number,
+        required: true
     },
-    SQL: {
-        type: String,
-        required: true,
-        max: 255,
-        min: 6
+    Goal: {
+        type: Boolean,
+        required: true
     },
-    Python: {
-        type: String,
-        required: true,
-        max: 1024,
-        min: 6
-    },
-    date: {
-        type: Date,
-        default: Date.now
+    Streak: {
+        type: Number,
+        required: true
     }
 });
+
+const Habit = mongoose.model('habit', habitSchema)
+
+module.exports = Habit;
